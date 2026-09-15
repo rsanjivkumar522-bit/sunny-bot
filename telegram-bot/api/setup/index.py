458544f60414acffe0f83080f8590c1fdf05cc67
rsanjivkumar522-bit <rsanjivkumar522@gmail.com>
2026-09-15 21:52:43 +0530

    Update index.py

--- telegram-bot/api/setup/index.py
+++ telegram-bot/api/setup/index.py
@@ -33,7 +33,7 @@ class handler(BaseHTTPRequestHandler):
             token = config.BOT_TOKENS[bot_number - 1]
 
             webhook_url = (
-                "https://sunny-bot-api-server-35ty.vercel.app/api"
+                "https://sunny-bot-api-server-728r.vercel.app/api"
                 f"?bot={bot_number}"
             )
 
